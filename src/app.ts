479c9e44143fa0378358b4eafa47e2392b076369
Chakib BOUABD <chakib.bouabd@gmail.com>
2019-12-07 14:43:47 +0100

    Added main routing controller and pokemon service

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,26 @@
-//src/app.ts
-
 import express, { Application } from 'express';
+import { Controller } from './main.controller';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 
 class App {
     public app: Application;
+    public pokeController: Controller
 
     constructor() {
         this.app = express();
         this.setConfig();
+        this.pokeController = new Controller(this.app);
     }
 
     private setConfig() {
-        //Allows us to receive requests with data in json format
+        // Allows us to receive requests with data in json format
         this.app.use(bodyParser.json({ limit: '50mb' }));
 
-        //Allows us to receive requests with data in x-www-form-urlencoded format
+        // Allows us to receive requests with data in x-www-form-urlencoded format
         this.app.use(bodyParser.urlencoded({ limit: '50mb', extended:true}));
 
-        //Enables cors   
+        // Enables cors   
         this.app.use(cors());
     }
 }
